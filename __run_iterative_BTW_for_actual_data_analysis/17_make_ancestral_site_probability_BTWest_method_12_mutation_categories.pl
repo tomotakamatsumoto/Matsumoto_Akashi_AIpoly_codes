cc1fe609483e94ade9337b7b89ec5e5ea743736a
tomotakamatsumoto <34563575+tomotakamatsumoto@users.noreply.github.com>
2018-06-29 15:33:13 +0900

    Update 17_make_ancestral_site_probability_BTWest_method_12_mutation_categories.pl

--- __run_iterative_BTW_for_actual_data_analysis/17_make_ancestral_site_probability_BTWest_method_12_mutation_categories.pl
+++ __run_iterative_BTW_for_actual_data_analysis/17_make_ancestral_site_probability_BTWest_method_12_mutation_categories.pl
@@ -230,6 +230,7 @@ for ($n=0;$n<=$#SEQ1-1;$n++) {
                 if ($preref ne "total") {
                     @preref = split //, $preref;
                     $ref_m = @preref[$target_node-1];
+                    $ref_ms = @preref[0];
                     
                     if ($ref_m eq $ref1) {
                         if ($ref_m eq "A") {
@@ -367,6 +368,7 @@ for ($n=0;$n<=$#SEQ1-1;$n++) {
                     $preref = $1;
                     @preref = split //, $preref;
                     $ref_m = @preref[$target_node-1];
+                    $ref_ms = @preref[0];
                     
                     if ($ref_m eq $ref1) {
                         if ($ref_m eq "A") {
